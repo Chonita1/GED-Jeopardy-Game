739b21526f22863a9bb46765405610ca176544de
Chonita1 <chonita@starpower.net>
2021-08-18 13:52:37 -0400

    "added 1 quest to content js file"

--- js/content.js
+++ js/content.js
@@ -1,1 +1,10 @@
 console.log("Let's play!")
+const questions = [
+    {
+        question: "Troy was devastated that he lost the wrestling championship, yet he found some ______ in the fact that it was his best effort.",
+        optionA: "solace",
+        optionB: "attitude",
+        optionC: "ambition",
+        optionD: "decimation",
+        correctOption: "optionA"
+    }]
