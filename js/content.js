d5bdb6d292e2033407c91b0dcd5b50ccb79f2700
Chonita1 <chonita@starpower.net>
2021-08-19 12:45:01 -0400

    "corrected syntax errors in quesitons array"

--- js/content.js
+++ js/content.js
@@ -2,22 +2,23 @@ console.log("Let's play!")
 const questions = [
     {
         question: "Troy was devastated that he lost the wrestling championship, yet he found some ______ in the fact that it was his best effort.",
-            a: "what is solace?",
+        answers: {
+        a: "what is solace?",
             b: "what is attitude?",
             c: "what is ambition?",
             d: "what is decimation?"
     },
-        {
     correctAnswer: "d"
     },
     {
         question: "The constitution can be altered through the passage of",
-        answers: 
-    {
+        answers: {
+
           a: "what is an amendment?",
           b: "what is a declaration?",
           c: "what is a mandate?",
           d: "what is a charter?"
     },
         correctAnswer: "a"
-}];
+    },
+];
