89392c2488e0c96acd9baf0f82b66b4a00ae2991
Chonita1 <chonita@starpower.net>
2021-08-18 23:09:02 -0400

    "created function to show slide"

--- js/app.js
+++ js/app.js
@@ -91,17 +91,34 @@ else {
     answerContainers[questionNumber].style.color = 'red';
 }
 });
-   
-
-
+ 
 }
-
 // display quiz right away
 buildQuiz();
 const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
 const slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0) {
+        previousButton.style.display = 'none';
+    }
+    else {
+        previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1) {
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+    }
+    else {
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+    }
+}
 
 //addEventListener to show question on click of choose button
 chooseButton.addEventListener('click', showQuestion);
