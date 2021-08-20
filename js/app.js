027acc77a5948fd5067bf4348dd77fdcd69ae68f
Chonita1 <chonita@starpower.net>
2021-08-20 19:33:57 -0400

    "committing final final version"

--- js/app.js
+++ js/app.js
@@ -6,12 +6,12 @@ console.log(questions.a);
 
 // I tried to wrap all of the game logic into this function, now
 //the questions don't show up
-const game = () => {} // stub
+const game = () => {}
 let player1Score = 0;
 let player2Score = 0;
 let playerID = 1;
 
-let timeInterval = 15;
+let timeInterval = 45;
 let timer;
 let timeLeft = timeInterval;
 
@@ -57,7 +57,6 @@ const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 const chooseButton = document.getElementById('choose');
-
 
 // function to build quiz & show results
 function buildQuiz(){
@@ -206,138 +205,3 @@ submitButton.addEventListener('click', showResults);
 //  hook nav buttons to the shownext and showprevious slide funciton
 previousButton.addEventListener("click", showPreviousSlide);
 nextButton.addEventListener("click", showNextSlide);
-
-
-// // function to shuffle an array
-// function shuffle(a) {
-//     for(let i = a.length - 1; i > 0; i--) {
-//        let j = Math.floor(Math.random() * (i + 1));
-//         a[i], a[j] = a[j], a[j];
-//     }
-//     return a;
-// }
-
-
-// class gedJeopardy {
-//     constructor(element, options={}) {
-//        //Game categories based on GED test categories
-//         this.useCategoryIds = options.useCategoryIds['language arts', 'science', 'social studies', 'math']; 
-
-//         this.categories = [];
-//         this.clues = {};
-
-// //States
-//         this.currentClue = null;
-//         this.score = 0;
-
-//         let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
-
-//         function handleQuestions() { 
-//             //to shuffle and push questions to shuffledQuestion
-//                 while(shuffledQuestions.length <= 2) {
-//                     const random = questions[Math.floor(Math.random() * questions.length)]
-//                     if (!shuffledQuestions.includes(random)) {
-//                         shuffledQuestions.push(random)
-//                     }
-//                 }
-//         }
-//         let questionNumber = 1 //holds the current question number
-//         let playerScore = 0  //holds the player score
-//         let wrongAttempt = 0 //amount of wrong answers picked by player
-//         let indexNumber = 0 //will be used in displaying next question
-        
-//         // function for displaying next question in the array to dom
-//         //also handles displaying players and quiz information to dom
-//         function nextQuestion(index) {
-//             handleQuestions()
-//             const currentQuestion = shuffledQuestions[index]
-        
-//         document.getElementById("question-number").innerHTML = questionNumber
-//         document.getElementById("player-score").innerHTML = playerScore
-//         document.getElementById("display-question").innerHTML = currentQuestion.question;
-//         document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
-//         document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
-//         document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
-//         document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
-//     }    
-
-    
-    
-    
-    
-    
-//             //Element querySelectors for board, score count, form, answers, clues, results
-//         this.boardElement = element.querySelector('.board');
-//         this.scoreCountElement = element.querySelector('score-count');
-//         this.formElement = element.querySelector('form');
-//         this.inputElement = element.querySelector('input[name=user-answer]');
-//         this.modalElement = element.querySelector('.card-modal');
-//         this.clueTextElement = element.querySelector('.clue-text');
-//         this.resultElement = element.querySelector('.result');
-//         this.resultTextElement = element.querySelector('.result_correct-answer-text');
-//         this.successTextElement = element.querySelector('.result_success');
-//         this.failTextElement = element.querySelector('.result_fail');
-//     }
-//     startGame() {
-//         this.boardElement.addEventListener("click", event =>{
-//             if (event.target.dataset.cluieID) {
-//                 this.handleClueClick(event);
-//             }
-//         });
-//         this.formElement.addEventListener("submit", event => {
-//             this.handleFormSubmit(event);
-//         });
-
-//         //make initial state of score
-//         this.updateScore(0);
-// }
-//     updateScore(change) {
-//         this.score += change;
-//         this.scoreCountElement.textContent = this.score;
-
-        
-
-//     // }   //Build category list
-//     let results = []
-//     //     result.forEach(result, categoryIndex) => {
-
-//         }
-// }
-// let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
-
-// function handleQuestions() { 
-//     //to shuffle and push questions to shuffledQuestion
-//         while(shuffledQuestions.length <= 2) {
-//             const random = questions[Math.floor(Math.random() * questions.length)]
-//             if (!shuffledQuestions.includes(random)) {
-//                 shuffledQuestions.push(random)
-//             }
-//         }
-// }
-// let questionNumber = 1 //holds the current question number
-// let playerScore = 0  //holds the player score
-// let wrongAttempt = 0 //amount of wrong answers picked by player
-// let indexNumber = 0 //will be used in displaying next question
-
-
-// // function for displaying next question in the array to dom
-// //also handles displaying players and quiz information to dom
-// // function NextQuestion(index) {
-// //     handleQuestions()
-// //     const currentQuestion = shuffledQuestions[index]
-
-
-
-// // }   //starting with a blank category
-//     let category = {
-//         title: result.title, clues: []
-//     }
-//     //adding all the clues by category
-//     let clues = shuffle(result.clues).splice(0.5).forEach((clue, index) => {
-//         console.log(clue)
-//     })
-
-// const game = gedJeopardy;
-// created a new instance of GED Jeopary
-// const game = new gedJeopardy (document.querySelector(".game"), {});
-// game.startGame();
--- js/content.js
+++ js/content.js
@@ -4,30 +4,30 @@ const questions = [
         question: "This sport is played by over 4 billion people in the world.",
         answers: {
             a: "what is basketball?",
-            b: "what is football?",
-            c: "what is soccer?",
-            d: "what is baseball?"
+            b: "What is football?",
+            c: "What is soccer?",
+            d: "What is baseball?"
     },
     correctAnswer: "c"
     },
     {
-        question: "Troy was devastated that he missed the three-pointer that would have clinched the championship wrestling match, yet he found some ______ in the fact that it was his best effort.",
+        question: "This word describes the feeling of comfort a dancer can have despite having lost a competition.",
         answers: {
-            a: "what is solace?",
-            b: "what is attitude?",
-            c: "what is ambition?",
-            d: "what is decimation?"
+            a: "What is solace?",
+            b: "What is attitude?",
+            c: "What is ambition?",
+            d: "What is decimation?"
     },
     correctAnswer: "a"
     },
     {
-        question: "The constitution can be altered through the passage of",
+        question: "The US Constitution can be altered through this process",
         answers: {
 
-          a: "what is a declaration?",
-          b: "what is an amendment?",
-          c: "what is a mandate?",
-          d: "what is a charter?"
+          a: "What is a declaration?",
+          b: "What is an amendment?",
+          c: "What is a mandate?",
+          d: "What is a charter?"
           
     },
         correctAnswer: "b"
@@ -37,25 +37,86 @@ const questions = [
                    so he sent Devon as his ____ to stir up trouble`,
         answers: {
 
-            a: "what is a posse",
-            b: "what is a surrogate",
-            c:  "what is a template",
-            d:  "what is a genome"
+            a: "What is a posse",
+            b: "What is a surrogate?",
+            c: "What is a template?",
+            d: "What is a client?"
     
     },
         correctAnswer: "b"
     },
     {
-        question: "The the value of 5 + 4 . 3 ÷ 6 – 1",
+        question: "Thhis is the value of 5 + 4 . 3 ÷ 6 – 1",
         answers: {
 
-          a: "5/9?",
-          b: "what is an amendment?",
-          c: "what is 6",
-          d: "what is 0?"
+          a: "What is 5/9?",
+          b: "What is 5?",
+          c: "What is 6",
+          d: "What is 0?"
 
     },
         correctAnswer: "c"
     },
+    {
+        question: `This court order grants police the authority
+         to enter private property to collect evidence for a crime`,
+        answers: {
 
+          a: "What is a search warrant?",
+          b: "What is an indictment?",
+          c: "What is amnesty?",
+          d: "What is a subpoena?"
+
+    },
+        correctAnswer: "a"
+    },
+    {
+        question: "This response accurately refers to the three phases or states of matter",
+        answers: {
+
+          a: "What are liquid, gas, ice?",
+          b: "What are liquid, solid, gas?",
+          c: "What is gas, liquid, slime",
+          d: "What is liquid, gas, hard?"
+
+    },
+        correctAnswer: "b"
+    },
+    {
+        question: "This response explains why the pressurize air inside a basketball causes it to bounce gravity and Newton’s Laws of Motion.",
+        answers: {
+
+          a: "What are heat and humidity?",
+          b: "What is gravity and Einstein's Theory of Relativity?",
+          c: "What is force and Pythagoreum's Theorum",
+          d: "What is gravity and Newton’s Laws of Motion?"
+
+    },
+        correctAnswer: "d"
+    },
+    {
+        question: `This consequence explains why a basketball player who jumps in the air to make a shot can appear to be suspended in mid-air
+                   during the high point of the jump.`,
+        answers: {
+
+          a: "What is projectile motion?",
+          b: "What is rotational motion?",
+          c: "What is exertional motion",
+          d: "What is high dimensional motion?"
+
+    },
+        correctAnswer: "a"
+    },
+    {
+        question: "This word is used to describe a talkative person.",
+        answers: {
+
+          a: "What is pithy?",
+          b: "What is verbose?",
+          c: "What is loquacious",
+          d: "What is evasive?"
+
+    },
+        correctAnswer: "c"
+    },
 ];
